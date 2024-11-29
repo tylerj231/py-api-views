fbb96e52f844a1a9268323d0e0f00daf26a4a5db
Mykola <jacktyler2391@gmail.com>
2024-11-29 17:18:11 +0200

    add patch method to actor, fix url from cinema-hall to cinema_hall

--- cinema/urls.py
+++ cinema/urls.py
@@ -6,12 +6,12 @@ from cinema.views import (MovieViewSet,
                           CinemaHallViewSet, ActorList, ActorDetail)
 
 router = routers.DefaultRouter()
-router.register("movies", MovieViewSet)
+router.register("movie", MovieViewSet)
 
 cinema_hall_list = CinemaHallViewSet.as_view(
     actions={
         "get": "list",
-        "post": "create"
+        "post": "create",
     }
 )
 
@@ -26,23 +26,23 @@ cinema_hall_detail = CinemaHallViewSet.as_view(
 urlpatterns = [
     path("",
          include(router.urls)),
-    path("genres/",
+    path("genre/",
          GenreList.as_view(),
          name="genre_list"),
-    path("genres/<int:pk>/",
+    path("genre/<int:pk>/",
          GenreDetail.as_view(),
          name="genre-detail"),
-    path("cinema-hall/",
+    path("cinema_hall/",
          cinema_hall_list,
          name="cinema_hall_list"),
-    path("cinema-hall/<int:pk>/",
+    path("cinema_hall/<int:pk>/",
          cinema_hall_detail,
          name="cinema_hall_detail"),
-    path("actors/",
+    path("actor/",
          ActorList.as_view(),
          name="actor_list"),
 
-    path("actors/<int:pk>/",
+    path("actor/<int:pk>/",
          ActorDetail.as_view(),
          name="actor-detail")
 ]
--- cinema/views.py
+++ cinema/views.py
@@ -46,6 +46,9 @@ class ActorDetail(
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
 
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
 
@@ -99,13 +102,12 @@ class GenreDetail(APIView):
 
 
 class CinemaHallViewSet(
-    viewsets.GenericViewSet,
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
     mixins.RetrieveModelMixin,
     mixins.UpdateModelMixin,
     mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
 ):
-
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
