5d9cc68f7d59b1dce7c7818b6de49d61ee732b30
Mykola <jacktyler2391@gmail.com>
2024-11-29 16:49:24 +0200

    fix issue with secret key error

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -11,10 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import dotenv
-import os
-
-dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = ("django-insecure-ru6ndfka@95_"
+              "(lysua8yhdjq@vpiqgv3yru4r)q3h4_u8x7dfy")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
